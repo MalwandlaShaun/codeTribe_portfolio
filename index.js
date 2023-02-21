5595e00f73f1e7326e25a065d9e15ed6833861ed
Malwandla Hlungwani <hlungwanimalwandlashaun@gmail.com>
2023-02-21 19:22:35 +0200

    we fix

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-// Define an array with the skills
-const skills = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "React",
-  "MongoDB",
-  "Express",
-  "Node",
+// // Define an array with the skills
+// const skills = [
+//   "HTML",
+//   "CSS",
+//   "JavaScript",
+//   "React",
+//   "MongoDB",
+//   "Express",
+//   "Node",
+// ];
+
+// // Get the skills list element from the DOM
+// const skillsList = document.getElementById("skills-list");
+
+// // Loop through the skills array and render them as list items
+// skills.forEach((skill) => {
+//   const listItem = document.createElement("li");
+//   listItem.textContent = skill;
+//   skillsList.appendChild(listItem);
+// });
+
+// About me page section
+
+const projects = [
+  "Netflix Clone",
+  "Spotify Clone",
+  "E-commerce website",
+  "Social media app",
+  "And many more",
 ];
 
-// Get the skills list element from the DOM
-const skillsList = document.getElementById("skills-list");
+const projectsList = document.getElementById("projects-list");
 
-// Loop through the skills array and render them as list items
-skills.forEach((skill) => {
+projects.forEach((project) => {
   const listItem = document.createElement("li");
-  listItem.textContent = skill;
-  skillsList.appendChild(listItem);
+  listItem.textContent = project;
+  projectsList.appendChild(listItem);
 });
